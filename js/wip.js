b1c0caf504b038a3fd38bc3859e24aa599e87aea
Sr. Galán <antoniogalanG15@gmail.com>
2024-11-15 15:36:40 +0100

    refactor: referencias sin uso eliminadas
    Se quitaron tanto los botones como sus mensajes de advertencia.

--- js/wip.js
+++ js/wip.js
@@ -3,16 +3,8 @@ function wip(id) {
     cuerpo = ""
 
     switch (id) {
-        case "htb":
-            cuerpo =    "Todavía no he hecho ninguna máquina de HackTheBox porque he usado más HTB Academy, así que mi perfil aún está vacío."
-            break;
-
         case "obsidian":
             cuerpo =    "Quiero hacer públicas mis notas de ciberseguridad, pero aún necesito tiempo para organizarlas y darles un formato adecuado (y dinero para pagar Obsidian Publish)."
-            break;
-
-        case "notion":
-            cuerpo =    "Creo que podría ser interesante tener una especie de portfolio en Notion, me parece muy útil, versátil y fácil de mantener, pero aún tengo que empezarlo."
             break;
 
         default:
